e27475d52e6cac7f6a4aca19b7d7c6e7c7cf06b0
Aaron Smith <aaroncosmith@gmail.com>
2020-01-31 13:10:33 -0500

    added edits, uncomplete frequency-python project.

---
+++ freq.py
@@ -0,0 +1,34 @@
+# Thisdict = {
+#     a = [1,2,3,4]
+#     b = [1,4,5,6]
+#     c = [1,4,4,2]
+#     d = [1,4,3,2]
+#     e = [1,2,3,4,5]
+# }
+
+# def some_function(i, j):
+#     for i in range(len(Thisdict)):
+#         if i.value 
+
+# some_function(a, a) **prints True**
+# some_function(a, b) **prints False**
+# some_function(a, c) **prints False**
+# some_function(a, d) **prints True**
+# some_function(e, a) **prints False**
+
+
+a = [1,2,3,4]
+b = [1,4,5,6]
+c = [1,4,4,2]
+d = [1,4,3,2]
+e = [1,2,3,4,5]
+
+i = {
+
+}
+j = {
+
+}
+
+def some some_function():
+    
